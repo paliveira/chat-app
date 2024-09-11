6b04d8702a32d6f9a7ce0e7b40f73e1c7e1b4227
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 12:28:56 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,4 @@
-// Importando as funções necessárias do Firebase
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+
 
 // Configuração do Firebase
 const firebaseConfig = {
@@ -14,9 +11,15 @@ const firebaseConfig = {
   measurementId: "G-K7RVRQP1LQ"
 };
 // Inicializando o Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-const realTimeDB = firebase.database();
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
 
-console.log("Firebase inicializado");
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const realTimeDB = getDatabase(app);
+
+// Exportando as instâncias
+export { auth, db, realTimeDB };
