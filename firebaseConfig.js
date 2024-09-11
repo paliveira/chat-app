b7b92404a055f0112c7b25ac4d7cee329c56a272
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 10:29:20 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,9 @@
+// Verifique se a versão do Firebase que você está usando é compatível com esta sintaxe
+// O código abaixo assume o uso do Firebase v9 ou superior, que usa módulos ES
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js';
+
 const firebaseConfig = {
   apiKey: "AIzaSyA2rZO8yXC6MnXcsP_m7V-RFjPnixj9ONc",
   authDomain: "chatdeaula-2c949.firebaseapp.com",
@@ -8,7 +14,10 @@ const firebaseConfig = {
   measurementId: "G-K7RVRQP1LQ"
 };
 
-// Inicializando o Firebase
-//firebase.initializeApp(firebaseConfig);
+// Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+console.log("Firebase inicializado");
+
