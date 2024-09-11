cc957d93983aded7dfbeb634061ea3181f12a899
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 08:46:42 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+// Inicializando o Firebase
+firebase.initializeApp(firebaseConfig);
