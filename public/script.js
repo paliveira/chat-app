149be00e5dbbb6d40f3801d0b51d04ae7b90144d
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 12:13:17 -0300

    Delete public directory

--- public/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app');
-    const login = document.createElement('script');
-    login.src = 'src/components/Login.js';
-    login.type = 'module';
-    app.appendChild(login);
-});
