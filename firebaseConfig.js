e43253cccf2c4319e1e8979b59d302bc5932b467
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 09:04:33 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
+  apiKey: "AIzaSyA2rZO8yXC6MnXcsP_m7V-RFjPnixj9ONc",
+  authDomain: "chatdeaula-2c949.firebaseapp.com",
+  projectId: "chatdeaula-2c949",
+  storageBucket: "chatdeaula-2c949.appspot.com",
   messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  appId: "chatdeaula-2c949"
 };
 
 // Inicializando o Firebase
