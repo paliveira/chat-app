7e037af532592aa7c55a80de097cdc085f73b146
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 12:49:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_DOMINIO_FIREBASE",
-    projectId: "SEU_PROJETO_FIREBASE",
-    storageBucket: "SEU_BUCKET",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
+      apiKey: "AIzaSyA2rZO8yXC6MnXcsP_m7V-RFjPnixj9ONc",
+      authDomain: "chatdeaula-2c949.firebaseapp.com",
+      projectId: "chatdeaula-2c949",
+      storageBucket: "chatdeaula-2c949.appspot.com",
+      messagingSenderId: "788797048931",
+      appId: "1:788797048931:web:67bc97cee7c80e75921056",
+      measurementId: "G-K7RVRQP1LQ"
 };
 
 // Inicializar Firebase
