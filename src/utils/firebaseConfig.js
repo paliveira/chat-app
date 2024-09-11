b167b1ddee248bfea3bf02edcbb025ccf5de49a5
paliveira <45421426+paliveira@users.noreply.github.com>
2024-09-11 10:51:12 -0300

    Update firebaseConfig.js

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,13 +1,18 @@
+// Importando as funções necessárias do Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJECT_ID.firebaseapp.com",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_PROJECT_ID.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
+  apiKey: "AIzaSyA2rZO8yXC6MnXcsP_m7V-RFjPnixj9ONc",
+  authDomain: "chatdeaula-2c949.firebaseapp.com",
+  projectId: "chatdeaula-2c949",
+  storageBucket: "chatdeaula-2c949.appspot.com",
+  messagingSenderId: "788797048931",
+  appId: "1:788797048931:web:67bc97cee7c80e75921056",
+  measurementId: "G-K7RVRQP1LQ"
 };
-
 // Inicializando o Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
